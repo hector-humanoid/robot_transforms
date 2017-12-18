c117df7cd29e1415ff3a83151d953e853c7810c3
Martin Oehler <mail@martinoehler.de>
2017-12-18 16:13:13 +0100

    added error messages when object wasn't properly initialized whitespace reformatting

--- include/robot_transforms/robot_transforms.h
+++ include/robot_transforms/robot_transforms.h
@@ -1,5 +1,5 @@
-#ifndef robot_transforms_H
-#define robot_transforms_H
+#ifndef ROBOT_TRANSFORMS__ROBOT_TRANSFORMS_H
+#define ROBOT_TRANSFORMS__ROBOT_TRANSFORMS_H
 
 #include <ros/ros.h>
 
@@ -8,103 +8,103 @@
 #include <moveit/robot_state/robot_state.h>
 
 namespace robot_tools {
-     /**
-     * @brief Stores the state of the robot (joint positions and root transformation) and computes forward kinematics.
-     */
-    class RobotTransforms {
-    public:
-        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-        /**
-         * @brief Default constructor
-         */
-        RobotTransforms() :
-            initialized_(false)
-        {}
+ /**
+ * @brief Stores the state of the robot (joint positions and root transformation) and computes forward kinematics.
+ */
+class RobotTransforms {
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-        /**
-         * @brief Initializes RobotTransforms
-         * @param robot_description_param_name (optional) Specifies the location of the robot
-         *                                     description on the parameter server.
-         */
-        void init(const std::string& robot_description_param_name = "robot_description");
+  /**
+   * @brief Default constructor
+   */
+  RobotTransforms() :
+      initialized_(false) {}
 
-        /**
-         * @brief Updates multiple joint states at once
-         * @param state_map Map between joint name and joint position
-         */
-        void updateState(const std::map<std::string, double>& state_map);
-        /**
-         * @brief Updates a single joint position
-         * @param joint_name Name of the joint
-         * @param state Position of the joint
-         */
-        void updateState(const std::string joint_name, double state);
+  /**
+   * @brief Initializes RobotTransforms
+   * @param robot_description_param_name (optional) Specifies the location of the robot
+   *                                     description on the parameter server.
+   */
+  void init(const std::string& robot_description_param_name = "robot_description");
 
-        /**
-         * @brief Returns the position of a joint
-         * @param joint_name Name of the joint
-         * @return Position of the joint
-         */
-        double getJointPosition(const std::string joint_name);
+  /**
+   * @brief Updates multiple joint states at once
+   * @param state_map Map between joint name and joint position
+   */
+  void updateState(const std::map<std::string, double>& state_map);
+  /**
+   * @brief Updates a single joint position
+   * @param joint_name Name of the joint
+   * @param state Position of the joint
+   */
+  void updateState(const std::string joint_name, double state);
 
-        /**
-         * @brief Updates the transformation to the root link
-         * @param transform Transform (rotation and translation) to the root link in world coordinates
-         */
-        void updateRootTransform(const Eigen::Affine3d& transform);
-        /**
-         * @brief Returns the Transform to the root link in world coordinates
-         * @return Transform (rotation and translation) to root link
-         */
-        const Eigen::Affine3d& getRootTransform();
+  /**
+   * @brief Returns the position of a joint
+   * @param joint_name Name of the joint
+   * @return Position of the joint
+   */
+  double getJointPosition(const std::string joint_name);
 
-        /**
-         * @brief Calculates forward kinematics from the root link to a given link
-         * @param link_name Name of the link
-         * @return Transformation matrix from root to link_name
-         */
-        Eigen::Affine3d getTransform(std::string link_name);
-        /**
-         * @brief Calculates forward kinematics between frames
-         * @param base_frame Reference frame
-         * @param target_frame Target frame
-         * @return Transformation matrix from base_frame to target_frame
-         */
-        Eigen::Affine3d getTransform(std::string base_frame, std::string target_frame);
+  /**
+   * @brief Updates the transformation to the root link
+   * @param transform Transform (rotation and translation) to the root link in world coordinates
+   */
+  void updateRootTransform(const Eigen::Affine3d& transform);
+  /**
+   * @brief Returns the Transform to the root link in world coordinates
+   * @return Transform (rotation and translation) to root link
+   */
+  const Eigen::Affine3d& getRootTransform();
 
-        /**
-         * @brief getChildLink Retrieves the child link name of a joint
-         * @param joint_name Name of the joint
-         * @return Returns child link name or an empty string if the joint wasn't found.
-         */
-        std::string getChildLink(std::string joint_name);
-        std::string getParentLink(std::string joint_name);
-    private:
-        /**
-         * @brief Loads the model (robot_description, robot_description_semantic) from parameter server
-         */
-        robot_model_loader::RobotModelLoaderPtr robot_model_loader_;
-        /**
-         * @brief Stores the robot model
-         */
-        robot_model::RobotModelPtr robot_model_ptr_;
-        /**
-         * @brief Stores the state of the robot and computes FK
-         */
-        robot_state::RobotStatePtr robot_state_ptr_;
+  /**
+   * @brief Calculates forward kinematics from the root link to a given link
+   * @param link_name Name of the link
+   * @return Transformation matrix from root to link_name
+   */
+  Eigen::Affine3d getTransform(std::string link_name);
+  /**
+   * @brief Calculates forward kinematics between frames
+   * @param base_frame Reference frame
+   * @param target_frame Target frame
+   * @return Transformation matrix from base_frame to target_frame
+   */
+  Eigen::Affine3d getTransform(std::string base_frame, std::string target_frame);
 
-        /**
-         * @brief Stores transform to root link
-         */
-        Eigen::Affine3d root_transform_;
+  /**
+   * @brief getChildLink Retrieves the child link name of a joint
+   * @param joint_name Name of the joint
+   * @return Returns child link name or an empty string if the joint wasn't found.
+   */
+  std::string getChildLink(std::string joint_name);
+  std::string getParentLink(std::string joint_name);
+private:
+  /**
+   * @brief Loads the model (robot_description, robot_description_semantic) from parameter server
+   */
+  robot_model_loader::RobotModelLoaderPtr robot_model_loader_;
+  /**
+   * @brief Stores the robot model
+   */
+  robot_model::RobotModelPtr robot_model_ptr_;
+  /**
+   * @brief Stores the state of the robot and computes FK
+   */
+  robot_state::RobotStatePtr robot_state_ptr_;
 
-        /**
-         * @brief True if init() has been called
-         */
-        bool initialized_;
+  /**
+   * @brief Stores transform to root link
+   */
+  Eigen::Affine3d root_transform_;
 
-    };
+  /**
+   * @brief True if init() has been called
+   */
+  bool initialized_;
+};
+
 }
 
 
--- src/robot_transforms.cpp
+++ src/robot_transforms.cpp
@@ -3,75 +3,84 @@
 namespace robot_tools {
 
 void RobotTransforms::init(const std::string &robot_description_param_name) {
-    ros::NodeHandle nh;
-    std::string robot_description;
-    if (!nh.getParam(robot_description_param_name, robot_description)) {
-        ROS_ERROR_STREAM("RobotTransforms couldn't find robot_description on " << nh.getNamespace() << "/" << robot_description_param_name <<". Init failed.");
-        return;
-    }
-    std::string robot_semantics;
-    if (!nh.getParam(robot_description_param_name + "_semantic", robot_semantics)) {
-        ROS_ERROR_STREAM("RobotTransforms couldn't find robot_description_semantic on " << nh.getNamespace() << "/" << robot_description_param_name << "_semantic" <<". Init failed.");
-        return;
-    }
+  ros::NodeHandle nh;
+  std::string robot_description;
+  if (!nh.getParam(robot_description_param_name, robot_description)) {
+    ROS_ERROR_STREAM("RobotTransforms couldn't find robot_description on " << nh.getNamespace() << "/" << robot_description_param_name <<". Init failed.");
+    return;
+  }
+  std::string robot_semantics;
+  if (!nh.getParam(robot_description_param_name + "_semantic", robot_semantics)) {
+    ROS_ERROR_STREAM("RobotTransforms couldn't find robot_description_semantic on " << nh.getNamespace() << "/" << robot_description_param_name << "_semantic" <<". Init failed.");
+    return;
+  }
 
-    robot_model_loader::RobotModelLoader::Options options(robot_description, robot_semantics);
-    options.load_kinematics_solvers_ = false;
-    robot_model_loader_.reset(new robot_model_loader::RobotModelLoader(options));
+  robot_model_loader::RobotModelLoader::Options options(robot_description, robot_semantics);
+  options.load_kinematics_solvers_ = false;
+  robot_model_loader_.reset(new robot_model_loader::RobotModelLoader(options));
 
-    robot_model_ptr_ = robot_model_loader_->getModel();
-    robot_state_ptr_.reset(new robot_state::RobotState(robot_model_ptr_));
-    robot_state_ptr_->setToDefaultValues();
-    root_transform_ = Eigen::Affine3d::Identity();
-    initialized_ = true;
+  robot_model_ptr_ = robot_model_loader_->getModel();
+  robot_state_ptr_.reset(new robot_state::RobotState(robot_model_ptr_));
+  robot_state_ptr_->setToDefaultValues();
+  root_transform_ = Eigen::Affine3d::Identity();
+  initialized_ = true;
 }
 
 void RobotTransforms::updateState(const std::map<std::string, double>& state_map) {
-    if (!initialized_) return;
-    robot_state_ptr_->setVariablePositions(state_map);
+  if (!initialized_) return;
+  robot_state_ptr_->setVariablePositions(state_map);
 }
 
 void RobotTransforms::updateState(const std::string joint_name, double state) {
-    if (!initialized_) return;
-    try {
-        robot_state_ptr_->setVariablePosition(joint_name, state);
-    } catch (moveit::Exception e) {
-        ROS_ERROR_STREAM_THROTTLE(1, "Couldn't update state for joint '" << joint_name << "'. Exception: " << e.what());
-    }
-}
-
-double RobotTransforms::getJointPosition(const std::string joint_name) {
-    if (!initialized_) return 0.0;
-    double position = 0;
-    try {
-        position = robot_state_ptr_->getVariablePosition(joint_name);
-    } catch (moveit::Exception e) {
-        ROS_ERROR_STREAM_THROTTLE(1, "Couldn't get state for joint '" << joint_name << "'. Exception: " << e.what());
-    }
-    return position;
+  if (!initialized_) return;
+  try {
+      robot_state_ptr_->setVariablePosition(joint_name, state);
+  } catch (moveit::Exception e) {
+      ROS_ERROR_STREAM_THROTTLE(1, "Couldn't update state for joint '" << joint_name << "'. Exception: " << e.what());
+  }
 }
 
 void RobotTransforms::updateRootTransform(const Eigen::Affine3d& transform) {
-    root_transform_ = transform;
+  root_transform_ = transform;
 }
 
-const Eigen::Affine3d& RobotTransforms::getRootTransform() {
-   return root_transform_;
+double RobotTransforms::getJointPosition(const std::string joint_name) {
+  if (!initialized_) {
+    ROS_ERROR_THROTTLE(1, "Robot transforms is not correctly initialized");
+    return 0.0;
+  }
+  double position = 0;
+  try {
+      position = robot_state_ptr_->getVariablePosition(joint_name);
+  } catch (moveit::Exception e) {
+      ROS_ERROR_STREAM_THROTTLE(1, "Couldn't get state for joint '" << joint_name << "'. Exception: " << e.what());
+  }
+  return position;
 }
 
 Eigen::Affine3d RobotTransforms::getTransform(std::string link_name) {
-    if (!initialized_) return Eigen::Affine3d::Identity();
-    try {
-        const Eigen::Affine3d& transform = robot_state_ptr_->getGlobalLinkTransform(link_name);
-        return transform;
-    } catch (moveit::Exception e) {
-        ROS_ERROR_STREAM_THROTTLE(1, "Couldn't get transform for link '" << link_name << "'. Exception: " << e.what());
-        return Eigen::Affine3d::Identity();
-    }
+  if (!initialized_) {
+    ROS_ERROR_THROTTLE(1, "Robot transforms is not correctly initialized");
+    return Eigen::Affine3d::Identity();
+  }
+  try {
+    const Eigen::Affine3d& transform = robot_state_ptr_->getGlobalLinkTransform(link_name);
+    return transform;
+  } catch (moveit::Exception e) {
+    ROS_ERROR_STREAM_THROTTLE(1, "Couldn't get transform for link '" << link_name << "'. Exception: " << e.what());
+    return Eigen::Affine3d::Identity();
+  }
+}
+
+const Eigen::Affine3d& RobotTransforms::getRootTransform() {
+  return root_transform_;
 }
 
 Eigen::Affine3d RobotTransforms::getTransform(std::string base_frame, std::string target_frame) {
-    if (!initialized_) return Eigen::Affine3d::Identity();
+  if (!initialized_) {
+    ROS_ERROR_THROTTLE(1, "Robot transforms is not correctly initialized");
+    return Eigen::Affine3d::Identity();
+  }
     return robot_state_ptr_->getGlobalLinkTransform(base_frame).inverse() * robot_state_ptr_->getGlobalLinkTransform(target_frame);
 }
 
